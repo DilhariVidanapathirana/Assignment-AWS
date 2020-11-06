033439e6143211169627599ebc5de6f95eb657e5
DilhariVidanapathirana <73744228+DilhariVidanapathirana@users.noreply.github.com>
2020-11-06 21:39:27 +0530

    initial commit

---
+++ src/main/java/lk/ijse/aws/assignment/AssignmentApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.aws.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//test comment
+@SpringBootApplication
+public class AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentApplication.class, args);
+    }
+
+}
